f7acb5337c6a49d76765f8710249ddad7bc4e179
Christelle Nkera <scnkera@gmail.com>
2024-12-06 14:30:15 -0500

    Updated components

---
+++ src/components/Cat.jsx
@@ -0,0 +1,12 @@
+const Cat = () => {
+    return (
+        <li>
+            <h3>Name of Cat</h3>
+            <p>🐈</p>
+            <button>Peat Cat</button>
+        </li>
+    );
+};
+
+export default Cat;
+
---
+++ src/components/CatList.jsx
@@ -0,0 +1,16 @@
+import Cat from './Cat'
+const CatList = () => {
+    return (
+        <>
+            <h2>Cat List</h2>
+            <ul>
+                <Cat />
+                <Cat />
+                <Cat />
+                <Cat />
+            </ul>
+        </>
+    );
+};
+
+export default CatList;
