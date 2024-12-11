299d2230eb04047986399489710063bd579ff26f
Christelle Nkera <scnkera@gmail.com>
2024-12-11 14:04:04 -0500

    Created onClick function

--- src/components/Cat.jsx
+++ src/components/Cat.jsx
@@ -1,14 +1,22 @@
 import PropTypes from 'prop-types';
+import {useState} from 'react';
 
 const Cat = ({name, personality, caretaker, color}) => {
+    const [petCount, setPetCount] = useState(0);
+
+    const petClicked = () => {
+        setPetCount(petCount => petCount + 1);
+    };
+
     return (
         <li>
             <h3>Name: {name}</h3>
             <h3>Color: {color}</h3>
             <h3>Personality: {personality}</h3>
             <h3>Caretaker: {caretaker}</h3>
+            <h3># of Pets: {petCount}</h3>
             <p>🐈</p>
-            <button>Pet Cat</button>
+            <button onClick={petClicked}>Pet Cat</button>
         </li>
     );
 };
